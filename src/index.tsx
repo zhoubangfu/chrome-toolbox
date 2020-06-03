9ea09bc82c840f540dc8bc18fb11d1b1aee855df
zhoubangfu <jiangji.zbf@raycloud.com>
2020-06-03 17:06:28 +0800

    add: 添加加解密\二维码模块

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,24 @@ import { Tabs } from 'antd'
 
 import StrTools from './views/StrTools'
 import RandomTools from './views/RandomTools'
+import QRCodeTools from './views/QRCodeTools'
+import EDCodeTools from './views/EDCodeTools'
 
 import './styles/global.scss'
 
 ReactDOM.render(
   <Tabs defaultActiveKey="1">
-    <Tabs.TabPane tab="字符工具" key="1">
+    <Tabs.TabPane tab="字符转换" key="1">
       <StrTools />
     </Tabs.TabPane>
     <Tabs.TabPane tab="随机值" key="2">
       <RandomTools />
+    </Tabs.TabPane>
+    <Tabs.TabPane tab="二维码" key="3">
+      <QRCodeTools />
+    </Tabs.TabPane>
+    <Tabs.TabPane tab="加解密" key="4">
+      <EDCodeTools />
     </Tabs.TabPane>
   </Tabs>,
   document.getElementById('popup')
---
+++ src/views/EDCodeTools/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RandomTools = () => {
+  return <div></div>
+}
+
+export default RandomTools
