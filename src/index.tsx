b276ebd87fb0cba9e2016b6986715d5d05649ace
zbf <zbfcqtl@163.com>
2020-06-14 14:00:07 +0800

    dev: 正则模块开发中...

--- src/index.tsx
+++ src/index.tsx
@@ -12,22 +12,30 @@ import EDCodeTools from './views/EDCode'
 import './styles/global.scss'
 
 ReactDOM.render(
-  <Tabs defaultActiveKey="2">
-    <Tabs.TabPane tab="字符转换" key="1">
-      <StrTools />
-    </Tabs.TabPane>
-    <Tabs.TabPane tab="随机值" key="2">
-      <RandomTools />
-    </Tabs.TabPane>
-    <Tabs.TabPane tab="正则测试" key="3">
-      <RegExpTools />
-    </Tabs.TabPane>
-    <Tabs.TabPane tab="二维码" key="4">
-      <QRCodeTools />
-    </Tabs.TabPane>
-    <Tabs.TabPane tab="加解密" key="5">
-      <EDCodeTools />
-    </Tabs.TabPane>
-  </Tabs>,
+  <React.StrictMode>
+    <Tabs defaultActiveKey="3">
+      <Tabs.TabPane tab="字符转换" key="1">
+        <StrTools />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="随机值" key="2">
+        <RandomTools />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="正则测试" key="3">
+        <RegExpTools />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="二维码" key="4">
+        <QRCodeTools />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="加解密" key="5">
+        <EDCodeTools />
+      </Tabs.TabPane>
+    </Tabs>
+    <footer className="footer">
+      源码地址：
+      <a href="https://github.com/varddd/chrome-toolbox" target="__blank">
+        https://github.com/varddd/chrome-toolbox
+      </a>
+    </footer>
+  </React.StrictMode>,
   document.getElementById('popup')
 )
--- src/views/RegExp/index.tsx
+++ src/views/RegExp/index.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Form, Input, Button } from 'antd'
 
 const RegExpTools = () => {
-  return <div>正则测试</div>
+  // 待匹配文本
+  const [text, setText] = useState<string>('')
+  // 正则文本
+  const [regexp, setRegexp] = useState<string>('')
+  // 替换文本
+  const [replaceText, setReplaceText] = useState<string>('')
+  const handleTest = (): void => {
+    console.log(text)
+  }
+
+  const handleReplace = (): void => {
+    console.log(replaceText)
+  }
+
+  return (
+    <div className="main">
+      <Form>
+        <Form.Item>
+          <Input.TextArea
+            placeholder="待匹配的文本"
+            rows={3}
+            value={text}
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setText(e.target.value)
+            }
+          />
+        </Form.Item>
+        <Form.Item style={{ marginBottom: 0 }}>
+          <Form.Item
+            name="regexp"
+            rules={[{ required: true }]}
+            style={{ display: 'inline-block', width: '70%' }}
+          >
+            <Input
+              value={regexp}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setRegexp(e.target.value)
+              }
+              placeholder="正则表达式"
+            />
+          </Form.Item>
+          <Form.Item style={{ display: 'inline-block', margin: '0 8px' }}>
+            <Button onClick={handleTest}>匹配</Button>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item>
+          <Input.TextArea placeholder="匹配结果" disabled rows={3} />
+        </Form.Item>
+        <Form.Item style={{ marginBottom: 0 }}>
+          <Form.Item
+            name="replaceText"
+            rules={[{ required: true }]}
+            style={{ display: 'inline-block', width: '70%' }}
+          >
+            <Input
+              value={replaceText}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setReplaceText(e.target.value)
+              }
+              placeholder="替换文本"
+            />
+          </Form.Item>
+          <Form.Item style={{ display: 'inline-block', margin: '0 8px' }}>
+            <Button onClick={handleReplace}>替换</Button>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item>
+          <Input.TextArea placeholder="替换结果" disabled rows={3} />
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
 export default RegExpTools
