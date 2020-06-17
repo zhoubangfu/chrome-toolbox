2dcea3a36b99bedd940b7c6f893b7d1adfe7b03b
zhoubangfu <jiangji.zbf@raycloud.com>
2020-06-17 17:24:31 +0800

    add  ✅正则模块

--- src/views/RegExp/index.tsx
+++ src/views/RegExp/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, Checkbox } from 'antd'
+
+const CheckboxGroup = Checkbox.Group
 
 const RegExpTools = () => {
   // 待匹配文本
@@ -9,12 +11,29 @@ const RegExpTools = () => {
   const [regexp, setRegexp] = useState<string>('')
   // 替换文本
   const [replaceText, setReplaceText] = useState<string>('')
+  // 正则状态
+  const [regexpType, setRegexpType] = useState<Array<any>>([])
+
+  // 匹配结果
+  const [matchResult, setMatchResult] = useState<string>('')
+  // 替换结果
+  const [replaceResult, setReplaceResult] = useState<string>('')
+
   const handleTest = (): void => {
-    console.log(text)
+    const generateReg = new RegExp(regexp, regexpType.join(''))
+    let matchArr: Array<any> | null = []
+
+    if (text && regexp) {
+      matchArr = text.match(generateReg)
+    }
+
+    const result = `${generateReg} 共${matchArr?.length}个结果：\n${matchArr?.join('\n')}`
+    setMatchResult(result)
   }
 
   const handleReplace = (): void => {
-    console.log(replaceText)
+    const generateReg = new RegExp(regexp, regexpType.join(''))
+    setReplaceResult(text.replace(generateReg, replaceText))
   }
 
   return (
@@ -34,14 +53,24 @@ const RegExpTools = () => {
           <Form.Item
             name="regexp"
             rules={[{ required: true }]}
-            style={{ display: 'inline-block', width: '70%' }}
+            style={{ display: 'inline-block', width: '50%' }}
           >
             <Input
               value={regexp}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                 setRegexp(e.target.value)
               }
-              placeholder="正则表达式"
+              placeholder="正则表达式."
+            />
+          </Form.Item>
+          <Form.Item style={{ display: 'inline-block', margin: '0 8px' }}>
+            <CheckboxGroup
+              options={[
+                { label: '全局', value: 'g' },
+                { label: '大小写', value: 'i' }
+              ]}
+              value={regexpType}
+              onChange={setRegexpType}
             />
           </Form.Item>
           <Form.Item style={{ display: 'inline-block', margin: '0 8px' }}>
@@ -49,7 +78,7 @@ const RegExpTools = () => {
           </Form.Item>
         </Form.Item>
         <Form.Item>
-          <Input.TextArea placeholder="匹配结果" disabled rows={3} />
+          <Input.TextArea placeholder="匹配结果" disabled rows={3} value={matchResult} />
         </Form.Item>
         <Form.Item style={{ marginBottom: 0 }}>
           <Form.Item
@@ -70,7 +99,7 @@ const RegExpTools = () => {
           </Form.Item>
         </Form.Item>
         <Form.Item>
-          <Input.TextArea placeholder="替换结果" disabled rows={3} />
+          <Input.TextArea placeholder="替换结果" rows={3} value={replaceResult} />
         </Form.Item>
       </Form>
     </div>
