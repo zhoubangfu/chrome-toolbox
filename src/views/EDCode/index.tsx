17f2d71c5e9467715e38fbe0b7aadc1f2a197562
zhoubangfu <jiangji.zbf@raycloud.com>
2020-07-13 20:06:09 +0800

    delete: 加密项

--- src/views/EDCode/index.tsx
+++ src/views/EDCode/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Tabs } from 'antd'
 
-import Base64 from './Base64'
 import Encode from './Encode'
-import Encrypt from './Encrypt'
 import Hash from './Hash'
 
 import styles from './style.module.scss'
@@ -12,19 +10,9 @@ const EncryptTools = () => {
   return (
     <div className="main">
       <Tabs defaultActiveKey="1" tabPosition="bottom">
-        <Tabs.TabPane tab="加解密" key="1">
-          <div className={styles.content}>
-            <Encrypt />
-          </div>
-        </Tabs.TabPane>
         <Tabs.TabPane tab="散列/哈希" key="2">
           <div className={styles.content}>
             <Hash />
-          </div>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="base64" key="3">
-          <div className={styles.content}>
-            <Base64 />
           </div>
         </Tabs.TabPane>
         <Tabs.TabPane tab="url转码" key="4">
