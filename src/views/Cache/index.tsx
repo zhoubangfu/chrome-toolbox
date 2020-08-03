dbbb8c7bee6259d3e43f7538d861f02de2ead047
zhoubangfu <jiangji.zbf@raycloud.com>
2020-08-03 14:50:02 +0800

    update: 修改清理缓存样式,添加提示文案

--- src/views/Cache/index.tsx
+++ src/views/Cache/index.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react'
-import { Form, Checkbox, Button, message, Radio, Popconfirm, Spin } from 'antd'
+import {
+  Form,
+  Checkbox,
+  Button,
+  message,
+  Radio,
+  Popconfirm,
+  Spin,
+  Row,
+  Col,
+  Alert
+} from 'antd'
 import { handleDo } from './handle'
 import styles from './style.module.scss'
 
@@ -24,8 +35,9 @@ const CacheTools = () => {
   }
 
   return (
-    <div className={styles['cache']}>
+    <div className="cache">
       <Spin tip="清理中，请销后..." spinning={spinning}>
+        <Alert message="数据清理操作不可逆，请谨慎使用！" type="warning" />
         <Form
           form={form}
           initialValues={{
@@ -39,21 +51,47 @@ const CacheTools = () => {
             rules={[{ required: true, message: '请至少选择一种类型!' }]}
           >
             <Checkbox.Group>
-              <Checkbox value="history" style={{ marginLeft: 8 }}>
-                浏览记录
-              </Checkbox>
-              <Checkbox value="cache">浏览器缓存</Checkbox>
-              <Checkbox value="cookies">Cookie</Checkbox>
-              <Checkbox value="downloads">下载历史</Checkbox>
-              <Checkbox value="formData">表单数据</Checkbox>
-              <Checkbox value="appcache">应用程序缓存</Checkbox>
-              <Checkbox value="fileSystems">文件系统数据</Checkbox>
-              <Checkbox value="localStorage">本地存储</Checkbox>
-              <Checkbox value="indexedDB">indexedDB</Checkbox>
-              <Checkbox value="serverBoundCertificates">证书</Checkbox>
-              <Checkbox value="pluginData">插件数据</Checkbox>
-              <Checkbox value="passwords">密码</Checkbox>
-              <Checkbox value="webSQL">WebSQL</Checkbox>
+              <Row>
+                <Col span={6}>
+                  <Checkbox value="history">浏览记录</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="cache">浏览器缓存</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="cookies">Cookie</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="downloads">下载历史</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="formData">表单数据</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="appcache">应用程序缓存</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="fileSystems">文件系统数据</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="localStorage">本地存储</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="indexedDB">indexedDB</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="serverBoundCertificates">证书</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="pluginData">插件数据</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="passwords">密码</Checkbox>
+                </Col>
+                <Col span={6}>
+                  <Checkbox value="webSQL">WebSQL</Checkbox>
+                </Col>
+              </Row>
             </Checkbox.Group>
           </Form.Item>
           <Form.Item
@@ -62,46 +100,70 @@ const CacheTools = () => {
             rules={[{ required: true, message: '请输入二维码内容!' }]}
           >
             <Radio.Group>
-              <Radio className={styles['rang-radio']} value="m10">
-                10分钟
-              </Radio>
-              <Radio className={styles['rang-radio']} value="m15">
-                15分钟
-              </Radio>
-              <Radio className={styles['rang-radio']} value="m30">
-                30分钟
-              </Radio>
-              <Radio className={styles['rang-radio']} value="h1">
-                1小时
-              </Radio>
-              <Radio className={styles['rang-radio']} value="h2">
-                2小时
-              </Radio>
-              <Radio className={styles['rang-radio']} value="h5">
-                5小时
-              </Radio>
-              <Radio className={styles['rang-radio']} value="d1">
-                1天
-              </Radio>
-              <Radio className={styles['rang-radio']} value="w1">
-                一周
-              </Radio>
-              <Radio className={styles['rang-radio']} value="M1">
-                一月
-              </Radio>
-              <Radio className={styles['rang-radio']} value="all">
-                全部
-              </Radio>
+              <Row>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="m10">
+                    10分钟
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="m15">
+                    15分钟
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="m30">
+                    30分钟
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="h1">
+                    1小时
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="h2">
+                    2小时
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="h5">
+                    5小时
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="d1">
+                    1天
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="w1">
+                    一周
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="M1">
+                    一月
+                  </Radio>
+                </Col>
+                <Col span={6}>
+                  <Radio className={styles['rang-radio']} value="all">
+                    全部
+                  </Radio>
+                </Col>
+              </Row>
             </Radio.Group>
           </Form.Item>
-          <Form.Item wrapperCol={{ span: 14, offset: 4 }}>
+          <Form.Item>
             <Popconfirm
               title="确认清理吗？？？"
               okText="确认"
               cancelText="取消"
               onConfirm={handelClean}
             >
-              <Button type="primary">清理</Button>
+              <Button type="primary" danger style={{ width: '100%' }}>
+                清理
+              </Button>
             </Popconfirm>
           </Form.Item>
         </Form>
