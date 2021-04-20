d9996fd15ddc787c8f856650fe01a3bd4dda6dc2
zhoubangfu <zbfcqtl@163.com>
2021-04-20 14:39:27 +0800

    feat: 保存tabs退出位置

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 import { Tabs } from 'antd'
@@ -13,37 +13,56 @@ import JsInsert from './views/JsInsert'
 
 import './styles/global.scss'
 
-ReactDOM.render(
-  <>
-    <Tabs defaultActiveKey="str-tool">
-      <Tabs.TabPane tab="字符转换" key="str-tool">
-        <StrTools />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="清理缓存" key="cache-tool">
-        <CacheTools />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="随机字符" key="random-tool">
-        <RandomTools />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="正则测试" key="reg-exp-tool">
-        <RegExpTools />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="加解密" key="edcode-tool">
-        <EDCodeTools />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="二维码" key="qrcode-tool">
-        <QRCodeTools />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="代码嵌入" key="console-tool">
-        <JsInsert />
-      </Tabs.TabPane>
-    </Tabs>
-    <footer className="footer">
-      源码地址：
-      <a href="https://github.com/zhoubangfu/chrome-toolbox" target="__blank">
-        https://github.com/zhoubangfu/chrome-toolbox
-      </a>
-    </footer>
-  </>,
-  document.getElementById('popup')
-)
+const App = () => {
+  const [activeKey, setKey] = useState('str-tool')
+
+  useEffect(() => {
+    chrome.storage.sync.get(['activeKey'], (result) => {
+      if (result.activeKey) {
+        setKey(result.activeKey)
+      }
+    })
+  }, [])
+
+  return (
+    <>
+      <Tabs
+        activeKey={activeKey}
+        onChange={(ak) => {
+          setKey(ak)
+          chrome.storage.sync.set({ activeKey: ak })
+        }}
+      >
+        <Tabs.TabPane tab="字符转换" key="str-tool">
+          <StrTools />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="清理缓存" key="cache-tool">
+          <CacheTools />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="随机字符" key="random-tool">
+          <RandomTools />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="正则测试" key="reg-exp-tool">
+          <RegExpTools />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="加解密" key="edcode-tool">
+          <EDCodeTools />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="二维码" key="qrcode-tool">
+          <QRCodeTools />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="代码嵌入" key="console-tool">
+          <JsInsert />
+        </Tabs.TabPane>
+      </Tabs>
+      <footer className="footer">
+        源码地址：
+        <a href="https://github.com/zhoubangfu/chrome-toolbox" target="__blank">
+          https://github.com/zhoubangfu/chrome-toolbox
+        </a>
+      </footer>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('popup'))
